f41a01e40033b6c8bf45144bc691b1a25b911746
Maud Gautier <maud.j.gautier@gmail.com>
2024-08-05 18:40:55 -0600

    feat(broker): add simple interface to listen for incoming connections

---
+++ src/broker.py
@@ -0,0 +1,32 @@
+import pickle
+import socket
+
+from src.id_generator import BrokerIdGenerator
+
+
+class Broker:
+    Host = str
+    Port = str
+
+    def __init__(self, host: Host, port: Port):
+        self.host = host
+        self.port = port
+        self.ID = BrokerIdGenerator(0)
+
+    def start(self):
+        """
+        Starts the broker and makes it listen for messages indefinitely.
+        """
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))
+            s.listen()
+            print(f"Broker {self.ID} listening on {self.host}:{self.port}")
+
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    print(f"Connected by {addr}")
+                    data = conn.recv(1024)
+                    message = pickle.loads(data)
+                    print(f"Received message on {self.host}:{self.port} (Broker {self.ID}): {message}")
